7f858973b305621ac95cdc48cd4c2f8ee87dcdc6
up202306317 <up202306317@up.pt>
2026-03-17 01:36:04 +0000

    State definition and basic game implementation

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # IA-g6
+
+
+# Lights Out
+
+## State Representation
+
+The game can be represented with a Square matrix with size N where lighted squares are '1' and closed squares are '0'.
+
+Final State: Matrix full of '0's 
+
+Operators:
+
+|  Name                         |  Preconditions                  |  Effects                                                                   |  Unitary Cost  | 
+|  Light Square M[i][j]         |  0 <= i,j  < N ; M[i][j] == '0' |  M[i][j] == '1', M[i][j+1]^=1, M[i][j-1]^=1 M[i-1][j]^=1 M[i+1][j]^=1      |       1        |
+|  Turn Off Square M[i][j]      |  0 <= i,j  < N ; M[i][j] == '1' |  M[i][j] == '0', M[i][j+1]^=1, M[i][j-1]^=1 M[i-1][j]^=1 M[i+1][j]^=1      |       1        |
+
---
+++ lightsOut.py
@@ -0,0 +1,105 @@
+import pygame
+import sys
+
+
+class LightsOutState:
+
+    def __init__(self, board):
+        self.board = board
+
+    def winningState(self):
+        return self.board == [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+
+    def move(self,row,column):
+
+        self.board[row][column] ^= 1
+
+        if(row > 0):
+            self.board[row-1][column] ^= 1
+        
+        if(column > 0):
+            self.board[row][column-1] ^= 1
+        
+
+        if(column < len(self.board)-1):
+            self.board[row][column+1] ^= 1
+
+        if(row < len(self.board)-1):
+            self.board[row+1][column] ^= 1
+
+    def printState(self):
+        for i in self.board:
+            print(i)
+
+def drawBoard(screen, board):
+    LIGHT = 110
+    ON_COLOR  = (255,215,0)
+    OFF_COLOR = (0,0,0)
+    BG_COLOR = (200,200,200)
+    size = len(board)
+
+    screen.fill(BG_COLOR)
+
+    for i in range(size):
+        for j in range(size):
+            x = 20 + (LIGHT+10) * i
+            y = 20 + (LIGHT+10) * j
+            color = ON_COLOR
+            if(board[i][j] == 0):
+                color = OFF_COLOR
+            pygame.draw.rect(screen, color, (x,y,LIGHT,LIGHT))
+    
+    pygame.display.flip()
+
+
+
+
+
+
+def play():
+
+    GAP       = 6
+    PADDING   = 30
+
+    state = LightsOutState([[1,1,0,0],
+                    [1,0,0,0],
+                    [0,0,0,1],
+                    [0,0,1,1]])
+    
+    size = len(state.board)
+    win_px = PADDING * 2 + size * 110 + (size - 1) * GAP
+    pygame.init()
+    screen = pygame.display.set_mode((win_px, win_px))
+    clock = pygame.time.Clock()
+
+    while not(state.winningState()):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit(); sys.exit()
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mx, my = event.pos
+                row = (mx - 20) // (110 + 10)
+                col = (my - 20) // (110 + 10)
+                if 0 <= row < size and 0 <= col < size:
+                    state.move(row, col)
+
+        drawBoard(screen,state.board)
+        clock.tick(60)
+    
+    print("You won")
+
+
+play()
+
+          
+    
+
+
+
+
+
+
+
+
+
