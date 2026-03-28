8573a4d28b77b3ac3c37824f15dc475d2ba1ec60
Tiago-Moreira73 <up202305180@up.pt>
2026-03-28 22:14:37 +0000

    algorithms done

--- lightsOut.py
+++ lightsOut.py
@@ -128,6 +128,68 @@ def heuristic2(state):
     #nr linhas totalmente desligadas
     return sum(1 for row in state.board if all(cell == 0 for cell in row))
 
+def greedy(initial_state, heuristic):
+    if initial_state.winningState():
+        return []
+    counter = 0
+    heap = [(heuristic(initial_state), counter, initial_state, [])]
+    visited = {initial_state}
+
+    while heap:
+        _, _, state, moves = heapq.heappop(heap)
+        for (r, c) in state.getPossibleMoves():
+            new_state = state.move(r, c)
+            if new_state.winningState():
+                return moves + [(r, c)]
+            if new_state not in visited:
+                visited.add(new_state)
+                counter+= 1
+                heapq.heappush(heap, (heuristic(new_state), counter, new_state, moves + [(r, c)]))
+    return None
+
+def astar(initial_state, heuristic):
+    if initial_state.winningState():
+        return []
+    counter = 0
+    heap = [(heuristic(initial_state), counter, initial_state, [])]
+    visited = {}
+
+    while heap:
+        _, _, state, moves = heapq.heappop(heap)
+        if state in visited : continue
+        visited[state] = len(moves)
+        if state.winningState():
+            return moves
+        for (r, c) in state.getPossibleMoves():
+            new_state = state.move(r, c)
+            if new_state not in visited:
+                g = len(moves) + 1
+                counter+= 1
+                heapq.heappush(heap, (g + heuristic(new_state), counter, new_state, moves + [(r, c)]))
+    return None
+
+
+def weighted_astar(initial_state, heuristic, w=2):
+    if initial_state.winningState():
+        return []
+    counter = 0
+    heap = [(w * heuristic(initial_state), counter, initial_state, [])]
+    visited = {}
+
+    while heap:
+        _, _, state, moves = heapq.heappop(heap)
+        if state in visited : continue
+        visited[state] = len(moves)
+        if state.winningState():
+            return moves
+        for (r, c) in state.getPossibleMoves():
+            new_state = state.move(r, c)
+            if new_state not in visited:
+                g = len(moves) + 1
+                counter+= 1
+                heapq.heappush(heap, (g + w * heuristic(new_state), counter, new_state, moves + [(r, c)]))
+    return None
+
 def randomBoard(n = 4, moves = 10):
     board = [[0]*n for _ in range(n)]
     state = LightsOutState(board)
@@ -206,6 +268,47 @@ def play():
                     sol = ucs(initial_state)
                     if sol:
                         state = LightsOutState(copy.deepcopy(initial_state.board))
+                        pc_moves = list(sol)
+                        pc_timer = 0
+                if event.key == pygame.K_1:      # Greedy heuristic1
+                    sol = greedy(initial_state, heuristic1)
+                    if sol:
+                        state    = LightsOutState(copy.deepcopy(initial_state.board))
+                        pc_moves = list(sol)
+                        pc_timer = 0
+
+                if event.key == pygame.K_2:      # Greedy heuristic2
+                    sol = greedy(initial_state, heuristic2)
+                    if sol:
+                        state    = LightsOutState(copy.deepcopy(initial_state.board))
+                        pc_moves = list(sol)
+                        pc_timer = 0
+
+                if event.key == pygame.K_3:      # A* heuristic1
+                    sol = astar(initial_state, heuristic1)
+                    if sol:
+                        state    = LightsOutState(copy.deepcopy(initial_state.board))
+                        pc_moves = list(sol)
+                        pc_timer = 0
+
+                if event.key == pygame.K_4:      # A* heuristic2
+                    sol = astar(initial_state, heuristic2)
+                    if sol:
+                        state    = LightsOutState(copy.deepcopy(initial_state.board))
+                        pc_moves = list(sol)
+                        pc_timer = 0
+
+                if event.key == pygame.K_5:      # Weighted A* heuristic1
+                    sol = weighted_astar(initial_state, heuristic1)
+                    if sol:
+                        state    = LightsOutState(copy.deepcopy(initial_state.board))
+                        pc_moves = list(sol)
+                        pc_timer = 0
+
+                if event.key == pygame.K_6:      # Weighted A* heuristic2
+                    sol = weighted_astar(initial_state, heuristic2)
+                    if sol:
+                        state    = LightsOutState(copy.deepcopy(initial_state.board))
                         pc_moves = list(sol)
                         pc_timer = 0
             if event.type == pygame.MOUSEBUTTONDOWN:
