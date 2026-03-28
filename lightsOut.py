3c8e6f0ab652ecde282c94861e9b842f1ac615bf
Tiago-Moreira73 <up202305180@up.pt>
2026-03-28 16:48:10 +0000

    implement bfs and dfs

--- lightsOut.py
+++ lightsOut.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
-
+from collections import deque
+import copy
 
 class LightsOutState:
 
@@ -8,29 +9,73 @@ class LightsOutState:
         self.board = board
 
     def winningState(self):
-        return self.board == [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+        return all(cell == 0 for row in self.board for cell in row)
 
     def move(self,row,column):
-
-        self.board[row][column] ^= 1
+        new_board = copy.deepcopy(self.board)
+        new_board[row][column] ^= 1
 
         if(row > 0):
-            self.board[row-1][column] ^= 1
+            new_board[row-1][column] ^= 1
         
         if(column > 0):
-            self.board[row][column-1] ^= 1
+            new_board[row][column-1] ^= 1
         
 
         if(column < len(self.board)-1):
-            self.board[row][column+1] ^= 1
+            new_board[row][column+1] ^= 1
 
         if(row < len(self.board)-1):
-            self.board[row+1][column] ^= 1
+            new_board[row+1][column] ^= 1
+        return LightsOutState(new_board)
 
     def printState(self):
         for i in self.board:
             print(i)
 
+    def getPossibleMoves(self):
+        n = len(self.board)
+        return [(r, c) for r in range(n) for c in range(n)]
+    
+    def __eq__(self, other):
+        return isinstance(other, LightsOutState) and self.board == other.board
+
+    def __hash__(self):
+        return hash(tuple(tuple(row) for row in self.board))
+
+
+def bfs(initial_state):
+    if initial_state.winningState():
+        return []
+    queue = deque([(initial_state, [])])
+    visited = {initial_state}
+    while queue:
+        state, moves = queue.popleft()
+        for (r, c) in state.getPossibleMoves():
+            new_state = state.move(r, c)
+            if new_state.winningState(): 
+                return moves + [(r,c)]
+            if new_state not in visited:
+                visited.add(new_state)
+                queue.append((new_state, moves + [(r,c)]))
+    return None
+
+def dfs(initial_state, max_depth=20):
+    if initial_state.winningState():
+        return []
+    stack = [(initial_state, [], frozenset([initial_state]))]
+
+    while stack:
+        state, moves, path_visited = stack.pop()
+        if len(moves) >= max_depth: continue
+        for(r, c) in state.getPossibleMoves():
+            new_state = state.move(r,c)
+            if new_state.winningState(): 
+                return moves + [(r,c)]
+            if new_state not in path_visited:
+                stack.append((new_state, moves + [(r,c)], path_visited | {new_state}))
+    return None
+    
 def drawBoard(screen, board, on_img, off_img):
     LIGHT = 110
     ON_COLOR  = (255,215,0)
@@ -42,19 +87,14 @@ def drawBoard(screen, board, on_img, off_img):
 
     for i in range(size):
         for j in range(size):
-            x = 20 + (LIGHT+10) * i
-            y = 20 + (LIGHT+10) * j
+            x = 20 + (LIGHT+10) * j
+            y = 20 + (LIGHT+10) * i
             if board[i][j] == 0:
                 screen.blit(on_img, (x, y))
             else:
                 screen.blit(off_img, (x, y))
     
     pygame.display.flip()
-
-
-
-
-
 
 def play():
 
@@ -83,10 +123,10 @@ def play():
             
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mx, my = event.pos
-                row = (mx - 20) // (110 + 10)
-                col = (my - 20) // (110 + 10)
+                col = (mx - 20) // (110 + 10)
+                row = (my - 20) // (110 + 10)
                 if 0 <= row < size and 0 <= col < size:
-                    state.move(row, col)
+                    state = state.move(row, col)
 
         drawBoard(screen, state.board, on_img, off_img)
         clock.tick(60)
